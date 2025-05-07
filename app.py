c2c7d32ccb867a1df0c82d71f0d7477b8cc7a48e
S.B <30941141+s-b-repo@users.noreply.github.com>
2025-05-07 13:51:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, send_file
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return send_file(os.path.join(os.path.dirname(__file__), "index.html"))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
